4f926a484c7dd89b29ad7ef278df0186b0a25098
vysf <yusufumar@student.untan.ac.id>
2023-02-13 13:45:08 +0700

    no testing for storage

--- src/Infrastructures/database/storage/StorageService.js
+++ src/Infrastructures/database/storage/StorageService.js
@@ -1,33 +1,33 @@
-// /* eslint-disable camelcase */
-// const fs = require('fs');
-// const UploadRepository = require('../../../Domains/uploads/UploadRepository');
+/* eslint-disable camelcase */
+const fs = require('fs');
+const UploadRepository = require('../../../Domains/uploads/UploadRepository');
 
-// class StorageService extends UploadRepository {
-//   constructor(folder) {
-//     super();
-//     this._folder = folder;
+class StorageService extends UploadRepository {
+  constructor(folder) {
+    super();
+    this._folder = folder;
 
-//     if (!fs.existsSync(folder)) {
-//       fs.mkdirSync(folder, { recursive: true });
-//     }
-//   }
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder, { recursive: true });
+    }
+  }
 
-//   // fungsi dengan nama writeFile yang menerima satu parameter
-//   // yaitu file yang merupakan Readable dan objek meta yang
-//   // mengandung informasi dari berkas yang akan ditulis seperti
-//   // nama berkas, content-type, dan sebagainya.
-//   writeFile(file, meta) {
-//     const filename = +new Date() + meta.filename;
-//     const path = `${this._folder}/${filename}`;
+  // fungsi dengan nama writeFile yang menerima satu parameter
+  // yaitu file yang merupakan Readable dan objek meta yang
+  // mengandung informasi dari berkas yang akan ditulis seperti
+  // nama berkas, content-type, dan sebagainya.
+  writeFile(file, meta) {
+    const filename = +new Date() + meta.filename;
+    const path = `${this._folder}/${filename}`;
 
-//     const fileStream = fs.createWriteStream(path);
+    const fileStream = fs.createWriteStream(path);
 
-//     return new Promise((resolve, reject) => {
-//       fileStream.on('error', (error) => reject(error));
-//       file.pipe(fileStream);
-//       file.on('end', () => resolve(filename));
-//     });
-//   }
-// }
+    return new Promise((resolve, reject) => {
+      fileStream.on('error', (error) => reject(error));
+      file.pipe(fileStream);
+      file.on('end', () => resolve(filename));
+    });
+  }
+}
 
-// module.exports = StorageService;
+module.exports = StorageService;
