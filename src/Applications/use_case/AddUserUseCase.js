2a2d84add9b177b7b364988e00119ad487edf447
vysf <yusufumar@student.untan.ac.id>
2023-03-19 13:38:35 +0700

    menambah dokumentasi addUserUseCase

--- src/Applications/use_case/AddUserUseCase.js
+++ src/Applications/use_case/AddUserUseCase.js
@@ -1,11 +1,27 @@
 const RegisterUser = require('../../Domains/users/entities/RegisterUser');
 
+/**
+ * @class AddUserUseCase
+ * @description Alur bisnis untuk menambahkan user baru
+ */
 class AddUserUseCase {
+  /**
+   * kontruktor modul yang dibutuhkan
+   * @param {object} userRepository repository user
+   * @param {object} passwordHash password hash
+   * @see {@link src/infrastructures/repository}
+   * @see {@link src/infrastructures/security}
+   */
   constructor({ userRepository, passwordHash }) {
     this._userRepository = userRepository;
     this._passwordHash = passwordHash;
   }
 
+  /**
+   * triger untuk menjalankan alur bisnis
+   * @param {object} useCasePayload payload berupa username, password, fullname
+   * @returns {object} RegisteredUser
+   */
   async execute(useCasePayload) {
     const registerUser = new RegisterUser(useCasePayload);
     await this._userRepository.verifyAvailableUsername(registerUser.username);
