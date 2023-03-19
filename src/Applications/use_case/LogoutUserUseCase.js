24a1e99dcf5e1ee327d6d65c8be0eb6d9585258b
vysf <yusufumar@student.untan.ac.id>
2023-03-19 14:05:53 +0700

    menambahkan dokumentasi logout use case

--- src/Applications/use_case/LogoutUserUseCase.js
+++ src/Applications/use_case/LogoutUserUseCase.js
@@ -1,18 +1,46 @@
 /* eslint-disable class-methods-use-this */
+
+/**
+ * @class LogoutUserUseCase
+ * @classdesc alur bisnis untuk logout user
+ */
 class LogoutUserUseCase {
+  /**
+   * kontruktor untuk dependesi yang dibutuhkan
+   * @param {object} authenticationRepository authentikasi service
+   * @see {@link src/infrastructures/repository}
+   */
   constructor({
     authenticationRepository,
   }) {
     this._authenticationRepository = authenticationRepository;
   }
 
+  /**
+   * Triger untuk menjalankan alur bisnis.\
+   * Adapun algoritma yang berjalan adalah:
+   * 1. melakukan cek `useCasePayload` agas sesuai dengan input yang diinginkan
+   * 2. melakukan cek ketersedian refresh token didalam database
+   * 3. menghapus refresh token yang ada didalam database
+   *
+   * @param {object} useCasePayload payload berupa refresh token
+   */
   async execute(useCasePayload) {
+    // #1
     this._validatePayload(useCasePayload);
     const { refreshToken } = useCasePayload;
+
+    // #2
     await this._authenticationRepository.checkAvailabilityToken(refreshToken);
+
+    // #3
     await this._authenticationRepository.deleteToken(refreshToken);
   }
 
+  /**
+   * melakukan validasi payload: payload tidak boleh kosong dan harus tipe data `string`
+   * @param {object} payload berisi refresh token
+   */
   _validatePayload(payload) {
     const { refreshToken } = payload;
     if (!refreshToken) {
