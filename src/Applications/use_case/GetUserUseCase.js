18a9ed37637f16332a33aafcfbe841e40dfac8a1
vysf <yusufumar@student.untan.ac.id>
2023-03-19 14:49:07 +0700

    menambahkan dokimentasi ke get user use case

--- src/Applications/use_case/GetUserUseCase.js
+++ src/Applications/use_case/GetUserUseCase.js
@@ -1,17 +1,47 @@
 /* eslint-disable class-methods-use-this */
+/**
+ * @class GetUserUseCase
+ * @classdesc Alur bisnis untuk mendapatkan data seorang user
+ */
 class GetUserUseCase {
+  /**
+   * konstrutor depedensi yang dibutuhkan
+   * @param {object} userRepository repositori user servis
+   * @see {@link src/infrastructures/repository}
+   */
   constructor({ userRepository }) {
     this._userRepository = userRepository;
   }
 
+  /**
+   * Mendapatkan data seorang user.\
+   * Adapun algortima yang berjalan adalah:
+   * 1. melakukan cek pada `useCasePayload` agar sesuai dengan input yang diinginkan
+   * 2. mendapatkan id dari `useCasePayload`
+   * 3. mendapatkan data use menggunakan id dari database
+   * 4. mengirimkan data user
+   *
+   * @param {object} useCasePayload payload berupa id
+   * @returns {object} data user
+   */
   async execute(useCasePayload) {
+    // #1
     this._verifyPayload(useCasePayload);
+
+    // #2
     const { id } = useCasePayload;
+
+    // #3
     const user = await this._userRepository.getUserById(id);
 
+    // #4
     return user;
   }
 
+  /**
+   * melakukan vefisikasi payload: tidak boleh kosong dan harus berupa string
+   * @param {object} payload berisi id
+   */
   _verifyPayload(payload) {
     const { id } = payload;
 
