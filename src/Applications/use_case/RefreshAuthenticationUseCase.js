b81060950f8808bccdecbb78d35a494235e8cfa6
vysf <yusufumar@student.untan.ac.id>
2023-03-19 15:13:26 +0700

    menambahkan dokumentasi ke refresh authentication use case

--- src/Applications/use_case/RefreshAuthenticationUseCase.js
+++ src/Applications/use_case/RefreshAuthenticationUseCase.js
@@ -1,5 +1,16 @@
 /* eslint-disable class-methods-use-this */
+/**
+ * @class RefreshAuthenticationUseCase
+ * @classdesc alur bisnis untuk mendapatkan access token baru
+ */
 class RefreshAuthenticationUseCase {
+  /**
+   * konstruksi depedensi yang dibutuhkan
+   * @param {object} authenticationRepository autentikasi repositori servis
+   * @param {object} authenticationTokenManager authentikasi token meneger service
+   * @see {@link src/infrastructures/repository}
+   * @see {@link src/infrastructures/security}
+   */
   constructor({
     authenticationRepository,
     authenticationTokenManager,
@@ -8,18 +19,43 @@ class RefreshAuthenticationUseCase {
     this._authenticationTokenManager = authenticationTokenManager;
   }
 
+  /**
+   * Triger untuk menjalankan alur bisnis.\
+   * Adapun algortima yang berjalan adalah:
+   * 1. melakukan cek `useCasePayload` agar sesuai dengan input yang diinginkan
+   * 2. mendapatkan refresh token dari `useCasePayload`
+   * 3. melakukan verifikasi refresh token
+   * 4. melakukan pengecekan ketersedian refresh token didalam database
+   * 5. melakukan decode refresh token utnuk mendapatkan id dan username
+   * 6. membuat access token baru dan mengirimkanya ke user
+   *
+   * @param {object} useCasePayload payload berupa refresh token
+   * @returns {string} access token
+   */
   async execute(useCasePayload) {
+    // #1
     this._verifyPayload(useCasePayload);
+
+    // #2
     const { refreshToken } = useCasePayload;
 
+    // #3
     await this._authenticationTokenManager.verifyRefreshToken(refreshToken);
+
+    // #4
     await this._authenticationRepository.checkAvailabilityToken(refreshToken);
 
+    // #5
     const { username, id } = await this._authenticationTokenManager.decodePayload(refreshToken);
 
+    // #6
     return this._authenticationTokenManager.createAccessToken({ username, id });
   }
 
+  /**
+   * melakukan verifikasi payload: tidak boleh kosong dan harus tipe data `string`
+   * @param {object} payload berisi refresh token
+   */
   _verifyPayload(payload) {
     const { refreshToken } = payload;
 
