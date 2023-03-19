077a9a04a5d1dd83ce3fdff3848ee0ccebe98138
vysf <yusufumar@student.untan.ac.id>
2023-03-19 13:43:05 +0700

    perbaikan dokumentasi adduserUseCase

--- src/Applications/use_case/AddUserUseCase.js
+++ src/Applications/use_case/AddUserUseCase.js
@@ -19,6 +19,11 @@ class AddUserUseCase {
 
   /**
    * triger untuk menjalankan alur bisnis
+   * adapaun algoritma yang berjalan adalah:
+   * 1. melakukan cek pada payload agar sesuai dengan imput yang diiginkan
+   * 2. melakukan cek ketersedian username. username harus unik
+   * 3. melakukan enkripsi terhadap password yang diberikan
+   * 4. menyimpan data pendaftaran user ke dalam database
    * @param {object} useCasePayload payload berupa username, password, fullname
    * @returns {object} RegisteredUser
    */
